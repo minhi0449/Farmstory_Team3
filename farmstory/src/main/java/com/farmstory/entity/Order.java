75baa52bc2f5547b835d2e276dcc6d9ec5bcd7b4
Hoon2e <sanghoon0120@gmail.com>
2024-09-17 11:37:38 +0900

    refactor: order관련 entity 리팩토링

--- farmstory/src/main/java/com/farmstory/entity/Order.java
+++ farmstory/src/main/java/com/farmstory/entity/Order.java
@@ -1,7 +1,9 @@
 package com.farmstory.entity;
 
+import com.farmstory.enums.PayMethod;
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 
@@ -18,13 +20,24 @@ public class Order {
     private int orderNo;
 
     private int point;
+    @Column(length = 45)
     private String receiver;
+    @Column(columnDefinition = "char(13)")
     private String receiverHp;
+    @Column(length = 20)
     private String zip;
+    @Column(length = 100)
     private String addr1;
+    @Column(length = 200)
     private String addr2;
-    private String payMethod;
+
+    @Enumerated(EnumType.STRING)
+    private PayMethod payMethod;
+
+    @Column(columnDefinition = "TEXT")
     private String etc;
+
+    @CreationTimestamp
     private LocalDateTime createAt;
 
     @ManyToOne
---
+++ farmstory/src/main/java/com/farmstory/enums/PayMethod.java
@@ -0,0 +1,8 @@
+package com.farmstory.enums;
+
+public enum PayMethod {
+    BANK_TRANSFER,  // 계좌이체
+    CREDIT_CARD,    // 신용카드
+    DEBIT_CARD,     // 체크카드
+    MOBILE_PAYMENT  // 휴대폰 결제
+}
