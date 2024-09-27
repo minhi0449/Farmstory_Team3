50c89d729e68a9bb333e5dd71796a7e3a0bc2fa3
wlgus5946 <wlgus5946@gmail.com>
2024-09-27 09:50:03 +0900

    modelmapper modify

--- farmstory/src/main/java/com/farmstory/config/AppInfo.java
+++ farmstory/src/main/java/com/farmstory/config/AppInfo.java
@@ -14,6 +14,6 @@ public class AppInfo {
     @Value("${spring.application.name}")
     private String appName;
 
-    @Value("${ext.version}")
-    private String appVersion;
+//    @Value("${ext.version}")
+//    private String appVersion;
 }
