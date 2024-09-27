9273d18b5ffbef37203663fd84e175ae27b98b2d
wlgus5946 <wlgus5946@gmail.com>
2024-09-27 18:50:01 +0900

    cartview

--- farmstory/src/main/java/com/farmstory/service/CartService.java
+++ farmstory/src/main/java/com/farmstory/service/CartService.java
@@ -1,40 +1,61 @@
 package com.farmstory.service;
 
-import com.farmstory.dto.CartDTO;
+import com.farmstory.dto.CartRequestDTO;
+import com.farmstory.dto.CartResponseDTO;
 import com.farmstory.entity.Cart;
 import com.farmstory.entity.Product;
 import com.farmstory.repository.CartRepository;
 import com.farmstory.repository.ProductRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Log4j2
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class CartService {
 
     private final ProductRepository productRepository;
     private final CartRepository cartRepository;
+    private final ModelMapper modelMapper;
 
 
-    public void insertCart(CartDTO cartDTO) {
-        Product product = productRepository.findById(cartDTO.getProduct_id());
-        log.info("Insert cart " + product);
-        Cart cart = cartDTO.toEntity();
-        cart.addProduct(product);
-//
-//        Cart cart = Cart.builder()
-//                .product(product)
-//                .count(cartDTO.getCount())
-//                .build();
+    public CartRequestDTO insertCart(CartRequestDTO cartRequestDTO) {
+        System.out.println("cartRequestDTO = " + cartRequestDTO);
+            Optional<Cart> existcart = cartRepository.findByProductProdNo(cartRequestDTO.getProduct_id());
+            Cart cart = null;
+            if(existcart.isPresent()) {
+                System.out.println("있음");
+                cart = existcart.get();
+                cart.increaseCount(cartRequestDTO.getCount());
 
-        cartRepository.save(cart);
-
-        cart.getCartNo();
+            }else {
+                System.out.println("없음");
+                Product product = productRepository.findById(cartRequestDTO.getProduct_id());
+                if(product != null) {
+                    cart = cartRequestDTO.toEntity();
+                    cart.addProduct(product);
+                    cartRepository.save(cart);
+                }
+            }
+        return modelMapper.map(cart, CartRequestDTO.class);
     }
 
 
+    public List<CartResponseDTO> selectAllCartByUid(String uid) {
+        List<Cart> carts = cartRepository.findByUid(uid);
+        log.info("optcart" + carts);
+
+        List<CartResponseDTO> cartDtos = carts.stream()
+                .map(cart -> modelMapper.map(cart, CartResponseDTO.class)) // DTO 변환
+                .collect(Collectors.toList());
+        return cartDtos;
+    }
 }
